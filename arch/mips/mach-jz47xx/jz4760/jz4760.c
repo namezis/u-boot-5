276a1b669eb9bbbd28981ac247f781f251c2e6b6
Jeremy Grosser <jeremy@synack.me>
2018-09-14 04:23:06 -0700

    Rework, no ddr config yet

--- arch/mips/mach-jz47xx/jz4760/jz4760.c
+++ arch/mips/mach-jz47xx/jz4760/jz4760.c
@@ -9,43 +9,152 @@
 
 #include <config.h>
 #include <common.h>
+#include <malloc.h>
 #include <asm/io.h>
 #include <mach/jz4760.h>
 #include <mmc.h>
 #include <spl.h>
+
+#define EXTAL	12000000
 
 #ifdef CONFIG_SPL_BUILD
 /* Pointer to the global data structure for SPL */
 DECLARE_GLOBAL_DATA_PTR;
 gd_t gdata __attribute__ ((section(".bss")));
 
+uint32_t pll_calc(uint32_t pll_out) {
+	// PLL M = (PLL_OUT/EXTAL * NO * N >> 1)
+	// PLL N = 2
+	// PLL OD = 1
+	uint8_t no;
+	uint8_t m, n, od;
+	uint32_t plcr;
+
+	if(pll_out > 1500) {
+		// PLL cannot be > 1500
+		return 0;
+	}else if(pll_out >= 624) {
+		od = 0;
+		no = 1;
+	}else if(pll_out >= 336) {
+		od = 1;
+		no = 2;
+	}else if(pll_out >= 168) {
+		od = 2;
+		no = 4;
+	}else if(pll_out >= 72) {
+		od = 3;
+		no = 8;
+	}else{
+		// PLL cannot be < 72
+		return 0;
+	}
+
+	n = 2;
+	m = (((pll_out / EXTAL) * no * n) >> 1);
+
+	if(m > 127 || m < 2) {
+		// Invalid PLL_M
+		return 0;
+	}
+
+	plcr = (m << 24) | (n << 18) | (od << 16);
+
+	return plcr;
+}
+
+void pll_init(void) {
+	uint32_t cfcr;
+	int n2FR[9] = {
+		0, 0, 1, 2, 3, 0, 4, 0, 5,
+	};
+
+	int div[6] = {1, 2, 4, 4, 4, 4};
+
+	cfcr = CPM_CPCCR_PCS |
+		(n2FR[div[0]] << CPM_CPCCR_CDIV_BIT) |
+		(n2FR[div[1]] << CPM_CPCCR_HDIV_BIT) |
+		(n2FR[div[2]] << CPM_CPCCR_H2DIV_BIT) |
+		(n2FR[div[3]] << CPM_CPCCR_PDIV_BIT) |
+		(n2FR[div[4]] << CPM_CPCCR_MDIV_BIT) |
+		(n2FR[div[5]] << CPM_CPCCR_SDIV_BIT);
+
+	// write REG_DDRC_CTRL 8 times to clear ddr fifo
+	REG_DDRC_CTRL = 0;
+	REG_DDRC_CTRL = 0;
+	REG_DDRC_CTRL = 0;
+	REG_DDRC_CTRL = 0;
+	REG_DDRC_CTRL = 0;
+	REG_DDRC_CTRL = 0;
+	REG_DDRC_CTRL = 0;
+	REG_DDRC_CTRL = 0;
+
+	if(EXTAL > 16000000) {
+		cfcr |= CPM_CPCCR_ECS;
+	}else{
+		cfcr &= ~CPM_CPCCR_ECS;
+	}
+
+	/* invert this if we're using MDDR */
+	cfcr |= CPM_CPCCR_MEM;
+
+	/* enable clocks */
+	cfcr |= CPM_CPCCR_CE;
+
+	/* PLL0 */
+	REG_CPM_CPPCR = (
+			pll_calc(CONFIG_SYS_MHZ * 1000000) |
+			(0x20 << CPM_CPPCR_PLLST_BIT) |
+			CPM_CPPCR_PLLEN);
+	/* wait for pll0 output stable ...*/
+	while (!(REG_CPM_CPPCR & CPM_CPPCR_PLLS));
+
+	/* configure dividers */
+	REG_CPM_CPCCR = cfcr;
+
+	/* 48 MHz USB Host Clock */
+	REG_CPM_UHCCDR = ((CONFIG_SYS_MHZ / 4) / 48) - 1;
+
+	/* PLL1 */
+	REG_CPM_CPPCR1 = (
+			pll_calc((CONFIG_SYS_MHZ * 1000000) / 4) |
+			CPM_CPPCR1_PLL1EN);
+	
+	__cpm_enable_pll_change();
+
+	/* wait for pll1 output stable ...*/
+	while(!(REG_CPM_CPPCR1 & CPM_CPPCR1_PLL1S));
+
+}
+
 void board_init_f(ulong dummy)
 {
-	typedef void __noreturn (*image_entry_noargs_t)(void);
-	struct mmc *mmc;
-	unsigned long count;
-	struct image_header *header;
-	int ret;
+	//typedef void __noreturn (*image_entry_noargs_t)(void);
+	//struct mmc *mmc;
+	//unsigned long count;
+	//struct image_header *header;
+	//int ret;
 
 	/* Set global data pointer */
 	gd = &gdata;
 
-	timer_init();
+	//timer_init();
 	pll_init();
-	sdram_init();
+	//sdram_init();
 	enable_caches();
 
 	/* Clear the BSS */
-	memset(__bss_start, 0, (char *)&__bss_end - __bss_start);
+	//memset(__bss_start, 0, (char *)&__bss_end - __bss_start);
 
 	gd->flags |= GD_FLG_SPL_INIT;
 
+	/*
 	ret = mmc_initialize(NULL);
 	if (ret)
 		hang();
 
 	mmc = find_mmc_device(BOOT_DEVICE_MMC1);
-	if (mmc == NULL)
+	if (ret)
 		hang();
 
 	ret = mmc_init(mmc);
@@ -65,56 +174,8 @@ void board_init_f(ulong dummy)
 		(image_entry_noargs_t)CONFIG_SYS_TEXT_BASE;
 
 	image_entry();
+	*/
 
-	hang();
+	//hang();
 }
 #endif /* CONFIG_SPL_BUILD */
-
-ulong board_get_usable_ram_top(ulong total_size)
-{
-	return CONFIG_SYS_SDRAM_BASE + (128 * 1024 * 1024);
-}
-
-int print_cpuinfo(void)
-{
-	printf("CPU:   Ingenic JZ4760\n");
-	return 0;
-}
-
-void _machine_restart(void)
-{
-	void __iomem *wdt_regs = (void __iomem *)WDT_BASE;
-
-	mdelay(100);
-
-	/*
-	 * Select the EXTAL as the timer clock input, and make each
-	 * WDT clock tick equal 4 ticks of the system clock.
-	 */
-	writew(WDT_TCSR_PRESCALE4 | WDT_TCSR_EXT_EN, wdt_regs + WDT_TCSR);
-
-	/* Reset the WDT counter to zero. */
-	writew(0, wdt_regs + WDT_TCNT);
-
-	/*
-	 * Reset after 4ms
-	 *
-	 *         1 sec    CONFIG_SYS_EXTAL ticks
-	 *  4ms * ------- * ---------------------- = Number of WDT clock ticks
-	 *        1000 ms           1 sec
-	 *
-	 * As noted above the number of system clock ticks have been
-	 * effectively multiplied by 4.  All that's left here for the
-	 * computation of WDT clock ticks is to divide by 1000
-	 * (one thousand).
-	 */
-	writew(JZ4760_SYS_EXTAL / 1000, wdt_regs + WDT_TDR);
-
-	//jz4760_tcu_wdt_start();
-
-	/* WDT start */
-	writeb(WDT_TCER_TCEN, wdt_regs + WDT_TCER);
-
-	for (;;)
-		;
-}
--- board/retromini/rs97/rs97.c
+++ board/retromini/rs97/rs97.c
@@ -8,40 +8,16 @@
  */
 
 #include <common.h>
-#include <serial.h>
-#include <asm/io.h>
-#include <asm/gpio.h>
 #include <mach/jz4760.h>
-//#include <mach/jz4760_dram.h>
-
-#define JZ_GPIO(bank, pin)     ((32 * (bank)) + (pin))
-
 
 int board_early_init_f(void)
 {
-	pll_init();
-	__gpio_as_uart2();
-	__cpm_start_uart2();
-
-	__gpio_as_uart1();
 	__cpm_start_uart1();
-
-	__gpio_as_msc0();
-
-	/* SYS_POWER_IND high (LED blue, VBUS on) */
-	gpio_direction_output(JZ_GPIO(5, 15), 1);
-
-	/* LEDs off */
-	gpio_direction_output(JZ_GPIO(2, 0), 0);
-	gpio_direction_output(JZ_GPIO(2, 1), 0);
-	gpio_direction_output(JZ_GPIO(2, 2), 0);
-	gpio_direction_output(JZ_GPIO(2, 3), 0);
-
+	__gpio_as_uart1();
 	return 0;
 }
 
-int dram_init(void)
-{
+int dram_init(void) {
 	return 0;
 }
 
@@ -51,16 +27,3 @@ int checkboard(void)
 	printf("Board: RETRO-GAME-V2.0 (retromini-rs97)\n");
 	return 0;
 }
-
-#ifdef CONFIG_SPL_BUILD
-
-#if defined(CONFIG_SPL_MMC_SUPPORT)
-int board_mmc_init(bd_t *bd)
-{
-	__gpio_as_msc0();
-	return jz_mmc_init((void __iomem *)MSC0_BASE);
-}
-#endif
-
-//H5PS1G63EFR-Y5C
-#endif
